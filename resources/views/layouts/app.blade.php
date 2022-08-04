d80c574f32e20246ce5ab1af38493eade6056edf
HeartFireY <yanweiqi1754989931@live.com>
2022-08-04 23:33:51 +0800

    Update lang support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -242,7 +242,7 @@
 
                     <div class="col-md-2 mx-auto">
                         <h5 class="title mb-4 mt-3 font-bold">{{__('footer.developments')}}</h5>
-                        <p class="mb-1"><a href="https://github.com/ZsgsDesign/NOJ">{{__('footer.opensource')}}</a></p>
+                        <p class="mb-1"><a href="https://github.com/JXNUACS/JXNUOJ-Based-on-NOJ-">{{__('footer.opensource')}}</a></p>
                         <p class="mb-1"><a href="#">{{__('footer.api')}}</a></p>
                     </div>
 
